ebcf21e9483c06453952bd22696691414966b9c6
Ystvlxst <88839467+Ystvlxst@users.noreply.github.com>
2022-02-02 21:21:33 +0500

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _prefabs;
+
+    private GameObject _spawned;
+    public event UnityAction RestartButtonClick;
+
+    private void Start()
+    {
+        gameObject.SetActive(true);
+        RestartButtonClick?.Invoke();
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private void CleanSpawner()
+    {
+        StopCoroutine(SpawnEnemy());
+
+        foreach (var item in _prefabs)
+            Destroy(item);
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        int spawnPoint = -5;
+        float minSpawnPointLevel = -1.2f;
+        float maxSpawnPointLevel = 1.2f;
+
+        yield return new WaitForSeconds(2f);
+
+        gameObject.SetActive(true);
+        _spawned = Instantiate(_prefabs[Random.Range(0, 3)], new Vector2(spawnPoint, Random.Range(minSpawnPointLevel, maxSpawnPointLevel)), Quaternion.identity);
+        Start();
+    }
+}
